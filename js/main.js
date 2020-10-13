079efbfb003ade047b783be9bcfba743aae479ff
Victoria <morriface@mail.ru>
2020-10-13 09:54:42 +0300

    недоработано соотв типа жилья и цены за ночь

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 'use strict';
 
-// const popupClose = window.card.mapCardTemplate.querySelector(`.popup__close`);
+const LIVES_TYPES_MIN_PRICE = {
+  bungalow: `0`,
+  flat: `1000`,
+  house: `5000`,
+  palace: `10000`
+
+};
 
 
 const titleElement = window.form.noticeElement.querySelector(`#title`);
 const priceElement = window.form.noticeElement.querySelector(`#price`);
+const livesType = window.form.noticeElement.querySelector(`#type`);
+const timein = window.form.noticeElement.querySelector(`#timein`);
+const timeout = window.form.noticeElement.querySelector(`#timeout`);
+
 
 window.disable.set(window.form.fieldsetArray);
 window.disable.set(window.form.mapFiltersArray);
@@ -17,6 +27,7 @@ window.form.roomNumberElement.addEventListener(`input`, window.form.dependenceOf
 
 titleElement.addEventListener(`input`, function () {
   const titleValueLength = titleElement.value.length;
+  console.log(titleValueLength);
   if (titleValueLength < 30) {
     titleElement.setCustomValidity(`Минамальное количество символов 30. Дополните заголовок.`);
   } else if (titleValueLength > 100) {
@@ -25,23 +36,27 @@ titleElement.addEventListener(`input`, function () {
     titleElement.setCustomValidity(``);
   }
 });
-/*
-const livesType = window.form.noticeElement.querySelector(`#type`);
 
-const livesTypesMinPrise = {
-  bungalow: `0`,
-  flat: `1000`,
-  house: `5000`,
-  palace: `10000`
+priceElement.addEventListener(`input`, function () {
+  const priceValue = priceElement.value;
+  if (priceValue > 1000000 || priceValue < 1000) {
+    priceElement.setCustomValidity(`Цена может варьироваться от 1000 до 1000000руб. Скорректируйте цену.`);
+  } else {
+    priceElement.setCustomValidity(``);
+  }
+  priceElement.reportValidity();
+});
 
-};
 
 livesType.addEventListener(`change`, function (evt) {
   const typeValue = evt.target.value;
-  const minPrice = livesTypesMinPrise[typeValue];
+  console.log(typeValue);
+  const minPrice = LIVES_TYPES_MIN_PRICE[typeValue];
+  console.log(minPrice);
   let priceValue = priceElement.value;
   if (priceValue < minPrice) {
     priceValue = minPrice;
+    priceElement.value = minPrice;
   }
   priceElement.addEventListener(`input`, function () {
 
@@ -51,17 +66,16 @@ livesType.addEventListener(`change`, function (evt) {
       priceElement.setCustomValidity(``);
     }
   });
+  priceElement.reportValidity();
 }
-);*/
+);
 
-priceElement.addEventListener(`input`, function () {
-  const priceValue = priceElement.value;
-  if (priceValue > 1000000 || priceValue < 1000) {
-    priceElement.setCustomValidity(`Цена может варьироваться от 1000 до 1000000руб. Скорректируйте цену.`);
-  } else {
-    priceElement.setCustomValidity(``);
-  }
-});
 
 window.form.fillAdressInput(window.position.PIN_WIDTH_HALF, window.position.PIN_HEIGHT / 2);
 
+timein.addEventListener(`change`, function () {
+  timeout.value = timein.value;
+});
+timeout.addEventListener(`change`, function () {
+  timein.value = timeout.value;
+});
